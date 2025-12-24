1b9ba4b85175db66f91137f101be1a5b8d2ecc65
Stanislav Fedii <stanislav.fedii@gmail.com>
2025-12-23 22:25:23 -0600

    feat: Update version numbers and implement onEnd event handling in Playwright Reporter

--- src/handlers/onEndHandler.ts
+++ src/handlers/onEndHandler.ts
@@ -1,1 +1,43 @@
-export function handleOnEndEvent() {}
+import { FullResult } from "@playwright/test/reporter";
+import { StanterpriseReporterOptions } from "../types";
+import * as grpc from "@grpc/grpc-js";
+import { reportUnary } from "../client/grpcClient";
+import { createDuration, createTimestampFromMs } from "../utils";
+import { TestRunEndEventRequest } from "@stanterprise/protobuf/testsystem/v1/events";
+import { TestStatus } from "@stanterprise/protobuf/testsystem/v1/common";
+
+function mapStatusToTestStatus(status: FullResult["status"]): TestStatus {
+  switch (status) {
+    case "passed":
+      return TestStatus.PASSED;
+    case "failed":
+      return TestStatus.FAILED;
+    case "timedout":
+      return TestStatus.TIMEDOUT;
+    case "interrupted":
+      return TestStatus.INTERRUPTED;
+    default:
+      return TestStatus.UNKNOWN;
+  }
+}
+
+export function handleOnEndEvent(
+  result: FullResult,
+  runId: string,
+  client: grpc.Client,
+  options: StanterpriseReporterOptions
+) {
+  const request = new TestRunEndEventRequest({
+    run_id: runId,
+    final_status: mapStatusToTestStatus(result.status),
+    start_time: createTimestampFromMs(result.startTime.getTime()),
+    duration: createDuration(result.duration),
+  });
+  reportUnary(
+    options,
+    client,
+    "/testsystem.v1.observer.TestEventCollector/ReportEnd",
+    request,
+    options.grpcTimeout
+  ).catch((e) => console.error("Failed to report end event", e));
+}
